12aca61a5298de78b7330bf1c187ac672d200fcc
keitaroi446 <149392536+keitaroi446@users.noreply.github.com>
2026-03-02 16:59:25 +0900

    fix BaseMsgReceiver global instance

--- stm32/nucleo_f446re/Src/user_uart_callback.cpp
+++ stm32/nucleo_f446re/Src/user_uart_callback.cpp
@@ -1,7 +1,7 @@
 #include "usart.h"
 #include "base_msg_receiver.hpp"
 
-extern BaseMsgReceiver base(huart2);
+extern BaseMsgReceiver base;
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
