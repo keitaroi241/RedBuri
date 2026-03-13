b1d9b590ff5c41d4d602757ea6dd06da101f6c6b
kei1m1 <149392536+kei1m1@users.noreply.github.com>
2026-03-13 11:37:31 +0900

    add struct MotorStatus

--- stm32/nucleo_f446re/lib/c620_can/c620_can.hpp
+++ stm32/nucleo_f446re/lib/c620_can/c620_can.hpp
@@ -1,28 +1,36 @@
 #pragma once
 
 #include <cstdint>
+#include "can.h"
 
 class C620CAN
 {
 public:
+    explicit C620CAN(CAN_HandleTypeDef* hcan);
     void init();
     void setCurrent(uint8_t motor_id, float current_amp);
     void sendCurrents();
-    void readMotorStatus();
-    float getAngle(uint8_t motor_id);
-    float getSpeed(uint8_t motor_id);
-    float getCurrent(uint8_t motor_id);
-    float getTemp(uint8_t motor_id);
+    void updateMotorStatus();
+    float getAngleDeg(uint8_t motor_id) const;
+    int16_t getSpeedRpm(uint8_t motor_id) const;
+    float getCurrentAmp(uint8_t motor_id) const;
+    int8_t getTempDegC(uint8_t motor_id) const;
 
 private:
-    static constexpr float LIMIT_CURRENT_AMP = 6.0f;        // ユーザー定義の電流制限[A]
-    static constexpr float MAX_CURRENT_AMP = 20.0f;         // 最大電流[A]
-    static constexpr int16_t MAX_CURRENT_RAW = 16384;       // 最大電流(16bit)
-    static constexpr uint16_t MAX_ANGLE_RAW = 8191;         // 最大角度(16bit)
+    static constexpr float LIMIT_CURRENT_AMP = 6.0f;    // ユーザー定義の電流制限[A]
+    static constexpr float MAX_CURRENT_AMP = 20.0f;     // 最大電流[A]
+    static constexpr uint16_t MAX_CURRENT_RAW = 16384;   // 最大電流(16bit)
+    static constexpr uint16_t MAX_ANGLE_RAW = 8191;     // 最大角度(16bit)
 
-    int16_t target_currents_raw[8] = {};
-    uint16_t angles_raw[8] = {};
-    int16_t speeds_rpm[8] = {};
-    int16_t currents_raw[8] = {};
-    uint8_t temps_degc[8] = {};    
+    struct MotorState
+    {
+        int16_t target_current_raw{};
+        float angle_deg{};
+        int16_t speed_rpm{};
+        float current_amp{};
+        int8_t temp_degC{};
+    };
+
+    CAN_HandleTypeDef* hcan_;
+    MotorState motors_[8]{};
 };
